c280ddf0f4f7db45ba5120529664bac08dd8023c
fermidrates <albertfarand@gmail.com>
2022-02-10 19:36:40 +0700

    feat: provide and map data from localStorage

--- src/pages/myPokemonList.tsx
+++ src/pages/myPokemonList.tsx
@@ -2,10 +2,20 @@ import React, { useEffect, useState } from "react";
 import Menu from "../component/menu";
 
 const MyPokemonList = () => {
+  const myPokemon_deserialized = JSON.parse(localStorage.getItem('myPokemon') || "");
   return (
     <div>
-      My Pokemon
-      <Menu />
+      <div className="d-flex pb-3">
+        <Menu />
+      </div>
+      <div className="d-flex justify-content-center m-5 p-2">
+        <h3>My Pokemon</h3>
+        {myPokemon_deserialized.pokemons.map((pokemon: any) => (
+          <div key={pokemon.id}>
+            <p>#{pokemon.id} {pokemon.nickname}</p>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
