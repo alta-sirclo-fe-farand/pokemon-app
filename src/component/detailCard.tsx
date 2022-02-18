bcd44527b698683231b8af7bb08597e552061c1f
fermidrates <albertfarand@gmail.com>
2022-02-19 02:33:21 +0700

    refactor: simplify if operation in map function, feat: add image props + styling

--- src/component/detailCard.tsx
+++ src/component/detailCard.tsx
@@ -3,36 +3,36 @@ import { ElementCard } from './elementCard/elementCard';
 
 export const DetailCard = ({
   name,
+  image,
   types,
   moves
 }: detailCardProps) => {
   return (
     <>
-      <div className="card" style={{width: "550px"}}>
-        <div className="card-body">
-          <h5 className="card-title">{name}</h5>
-          <h6>Type</h6>
-          {types
-            ? <div className="d-flex">
-              {types.map((type, index: number) => (
+      <div className="card" style={{width: "700px"}}>
+        <div className="row w-100">
+          <div className="w-25 mx-2 d-flex justify-content-center align-items-center">
+            <img src={image} alt="Pokemon Image" width="150px" height="150px" style={{border: "2px solid", borderRadius: "20px", padding: "10px"}}/>
+          </div>
+          <div className="card-body w-50">
+            <h5 className="card-title">{name}</h5>
+            <h6>Type</h6>
+            <div className="d-flex">
+              {types?.map((type, index: number) => (
                 <div key={index}>
                   <ElementCard name={type} />
                 </div>
               ))}
             </div>
-            : <div className="d-flex">{"Not Available"}</div>
-          }
-          <h6>Move</h6>
-          {moves
-            ? <div className="d-flex">
-              {moves.map((move, index: number) => (
+            <h6>Move</h6>
+            <div className="d-flex">
+              {moves?.map((move, index: number) => (
                 <div key={index} className="container">
                   {move}
                 </div>
               ))}
             </div>
-            : <div className="d-flex">{"Not Available"}</div>
-          }
+          </div>
         </div>
       </div>
     </>
