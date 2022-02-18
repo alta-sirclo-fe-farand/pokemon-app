81f2cfa347431762ae0713910b7b9df2f5a3be11
fermidrates <albertfarand@gmail.com>
2022-02-19 02:55:14 +0700

    chore: change == to ===

--- src/utils/handleIdConversion.tsx
+++ src/utils/handleIdConversion.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export const handleIdConversion = (id: number | string) => {
   const idConversion = 
-    (id.toString().length == 1)
+    (id.toString().length === 1)
     ? `00${id}`
-    : (id.toString().length == 2)
+    : (id.toString().length === 2)
     ? `0${id}`
     : `${id}`
   return idConversion;
